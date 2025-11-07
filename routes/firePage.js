53f78d2c331e2268a4c1830047ea94412064de14
Nata-N-5 <nnogueraf@academia.usbbog.edu.co>
2025-11-07 01:07:37 -0500

    subir prototiposteam

---
+++ routes/firePage.js
@@ -0,0 +1,34 @@
+const db = require("./firebase-init.js");
+
+
+let lastDoc = null;
+ 
+async function getFirstPage() {
+    const q = db.collection("user")
+        .limit(2);
+ 
+    const snap = await q.get();
+    if (snap.empty) { console.log('Sin datos'); return; }
+ 
+    snap.forEach(doc => console.log(doc.id, doc.data()));
+    lastDoc = snap.docs[snap.docs.length - 1];
+}
+ 
+async function getNextPage() {
+    if (!lastDoc) { console.log('Primero llama getFirstPage()'); return; }
+ 
+    const q = db.collection('user')
+        .startAfter(lastDoc)
+        .limit(2);
+ 
+    const snap = await q.get();
+    if (snap.empty) { console.log('No hay mas paginas'); return }
+ 
+    snap.forEach(doc => console.log(doc.id, doc.data()));
+    lastDoc = snap.docs[snap.docs.length - 1]
+}
+ 
+(async () => {
+  await getFirstPage();
+  await getNextPage();
+})();
